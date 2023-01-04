ed03abc2ca1168289c656381aa8e26dc8df037c2
vladimirivliev <vladimir198818@mail.ru>
2023-01-04 23:07:39 +0300

    Изменил код. Теперь он парсит все содержимое сайста Всеинструменты.

--- Parser.py
+++ Parser.py
@@ -6,14 +6,19 @@ from browser import Driver_Chrom
 class Parser:
     def __init__(self, brand: list = None):
         self.brand = brand
+        xpath_for_names = '//div[@data-qa="products-tile"]/a[@data-qa="product-name"]'
+        xpath_for_prices = '//div[@data-qa="products-tile"]//p[@data-qa="product-price-current"]'
+        xpath_for_last_page = '//a[@class="number"]'
+        url_for_find_all_links = 'https://spb.vseinstrumenti.ru/category/stroitelnyj-instrument-6474/'
+        xpath_for_find_all_links = "//a[@href = 'https://spb.vseinstrumenti.ru/category/akkumulyatornyj-instrument-2392/']//ancestor::div[1]"
 
     def parser_page(self, page: int = 1, link: str = '') -> list:
         url = link if page == 1 else f'{link}/page{page}'
         driver = Driver_Chrom().loadChrome(headless=True)
         driver.get(url)
         time.sleep(1)
-        names = driver.find_elements(By.XPATH, '//div[@data-qa="products-tile"]/a[@data-qa="product-name"]')
-        prices = driver.find_elements(By.XPATH, '//div[@data-qa="products-tile"]//p[@data-qa="product-price-current"]')
+        names = driver.find_elements(By.XPATH, self.xpath_for_names)
+        prices = driver.find_elements(By.XPATH, self.xpath_for_prices)
         all_names = list(map(lambda x: x.get_attribute('title'), names))
         all_prices = list(map(lambda x: x.text.strip(' р.').replace(' ', ''), prices))
         data_result = list(zip(all_names, all_prices))
@@ -26,7 +31,7 @@ class Parser:
         driver.get(link)
         time.sleep(1)
         try:
-            last_page = min(int(driver.find_elements(By.XPATH, '//a[@class="number"]')[-1].text), 100)
+            last_page = min(int(driver.find_elements(By.XPATH, self.xpath_for_last_page)[-1].text), 250)
         except:
             last_page = 1
         driver.close()
@@ -35,11 +40,9 @@ class Parser:
 
     def get_all_links(self) -> list:
         driver = Driver_Chrom().loadChrome(headless=True)
-        url = 'https://spb.vseinstrumenti.ru/category/stroitelnyj-instrument-6474/'
-        xpath = "//a[@href = 'https://spb.vseinstrumenti.ru/category/akkumulyatornyj-instrument-2392/']//ancestor::div[1]"
-        driver.get(url)
+        driver.get(self.url_for_find_all_links)
         time.sleep(0.5)
-        class_element = driver.find_elements(By.XPATH, xpath)[1].get_attribute('class')
+        class_element = driver.find_elements(By.XPATH, self.xpath_for_find_all_links)[1].get_attribute('class')
         xpath_for_all = f'//div[contains(@class, "{class_element}")]//a'
         all_links_driver = driver.find_elements(By.XPATH, xpath_for_all)
         all_links = set(map(lambda x: x.get_attribute('href'), all_links_driver))
